43654d12c5533fb89c11e23ed5f832732d64fb14
Overlord <daylandelange@gmail.com>
2020-03-11 09:32:32 +0100

    Reader works

--- ATM/login/src/login.cpp
+++ ATM/login/src/login.cpp
@@ -43,79 +43,91 @@ void setup()
 void loop()
 {
 
-	/*if (!cardAccess) {
-	//-------------------------------------CARD READING--------------------------------------------
-	// Look for new cards
-	if (!mfrc522.PICC_IsNewCardPresent())	{
-		return;
-	} // Select one of the cards
-	// if (!mfrc522.PICC_ReadCardSerial())	{
-	// 	return;
-	// }
+	if (!cardAccess)
+	{
+		//-------------------------------------CARD READING--------------------------------------------
+		// Look for new cards
+		if (!mfrc522.PICC_IsNewCardPresent())
+		{
+			return;
+		} // Select one of the cards
+		if (!mfrc522.PICC_ReadCardSerial())	{
+		 	return;
+		}
 
-	//Show UID on serial monitor
-	Serial.print("UID tag :");
-	String content = "";
-	byte letter;
+		//Show UID on serial monitor
+		Serial.print("UID tag :");
+		String content = "";
+		byte letter;
 
-	for (byte i = 0; i < mfrc522.uid.size; i++)	{
-		Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-		Serial.print(mfrc522.uid.uidByte[i], HEX);
-		content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-		content.concat(String(mfrc522.uid.uidByte[i], HEX));
-	}
+		for (byte i = 0; i < mfrc522.uid.size; i++)
+		{
+			Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+			Serial.print(mfrc522.uid.uidByte[i], HEX);
+			content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+			content.concat(String(mfrc522.uid.uidByte[i], HEX));
+		}
 
-	Serial.println();
-	Serial.print("Message : ");
-	content.toUpperCase();
-
-	if (content.substring(1) == "CA 4A F5 0B")	{ //change here the UID of the card/cards that you want to give access
-		Serial.println("Authorized access");
 		Serial.println();
-		cardAccess = true;
-	}	else	{
-		Serial.println(" Access denied");
-		delay(3000);
-		cardAccess = false;
+		Serial.print("Message : ");
+		content.toUpperCase();
+
+		if (content.substring(1) == "CA 4A F5 0B")
+		{ //change here the UID of the card/cards that you want to give access
+			Serial.println("Authorized access");
+			Serial.println();
+			cardAccess = true;
+		}
+		else
+		{
+			Serial.println(" Access denied");
+			analogWrite(redPin, 255);
+			delay(3000);
+			analogWrite(redPin, 0);
+			cardAccess = false;
+		}
 	}
-	}
-	*/
 
 	//-------------------------------------KEYPAD LOGIN--------------------------------------------
 
-	//if (cardAccess)	{
-	char key;
-	// correct ping
-	if (number >= 4 && code == pin)
+	if (cardAccess)
 	{
-		Serial.println("Logged into ATM");
-		number = 0;
-		code = "";
-		analogWrite(greenPin, 255);
-		analogWrite(redPin, 0);
-		return;
-	} // Incorrect ping
-	else if (number >= 4)
-	{
-		Serial.println("Wrong pin");
-		number = 0;
-		code = "";
-		analogWrite(redPin, 255);
-		analogWrite(greenPin, 0);
-		return;
-	}
+		char key;
+		// correct ping
+		if (number >= 4 && code == pin)
+		{
+			Serial.println("Logged into ATM");
+			number = 0;
+			code = "";
+			cardAccess = false;
+			analogWrite(greenPin, 255);
+			delay(3000);
+			analogWrite(greenPin, 0);
+			return;
+		} // Incorrect ping
+		else if (number >= 4)
+		{
+			Serial.println("Wrong pin");
+			number = 0;
+			code = "";
+			cardAccess = false;
+			analogWrite(redPin, 255);
+			delay(3000);
+			analogWrite(redPin, 0);
+			return;
+		}
 
-	key = keypad.getKey();
-	if (key)
-	{ // add key to total code
-		code += key;
-		number++;
-		Serial.print("key: ");
-		Serial.println(key);
-		Serial.print("code: ");
-		Serial.println(code);
-		Serial.print("nr. : ");
-		Serial.println(number);
+		key = keypad.getKey();
+		if (key)
+		{ // add key to total code
+			code += key;
+			number++;
+			Serial.print("key: ");
+			Serial.println(key);
+			Serial.print("code: ");
+			Serial.println(code);
+			Serial.print("nr. : ");
+			Serial.println(number);
+		}
 	}
-	//}
 }
