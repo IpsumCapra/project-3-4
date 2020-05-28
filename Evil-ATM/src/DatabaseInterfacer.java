0baad981af34e9c80acbb040da9c1ba70097b6cd
Daylan <daylandelange@gmail.com>
2020-05-28 19:59:49 +0200

    interfacer class V1

--- Evil-ATM/src/DatabaseInterfacer.java
+++ Evil-ATM/src/DatabaseInterfacer.java
@@ -2,6 +2,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 
+import org.apache.commons.text.StringEscapeUtils;
+import org.json.JSONObject;
+
 public class DatabaseInterfacer {
     private Socket socket;
     private DataInputStream dIn;
@@ -9,7 +12,7 @@ public class DatabaseInterfacer {
     private String ip;
     private int port;
 
-    private static final String[] errorMsg = {"404"};
+    private static final String[] errorMsg = { "404" };
 
     public DatabaseInterfacer(String ip, int port) {
         this.ip = ip;
@@ -27,33 +30,62 @@ public class DatabaseInterfacer {
         return true;
     }
 
-    public String[] getInformation(String accountNumber, String pin) {
+    public String[] getInformation(String accountNumber, String pin, String requestType) {
         if (!connect()) {
             return errorMsg;
         }
         try {
-            dOut.writeByte(1);
-            dOut.writeUTF(accountNumber);
-            dOut.writeUTF(pin);
-        } catch (Exception e) {
-            return errorMsg;
-        }
 
-        try {
-            switch(dIn.readByte()) {
-                case 1:
-                    return new String[] {
-                        dIn.readUTF(), //Voornaam
-                        dIn.readUTF(), //Tussenvoegsel als het er niet is is het ""
-                        dIn.readUTF(), //Achternaam
-                        String.valueOf(dIn.readInt()) //Balans
-                    };
-                case 0:
-                    return new String[] {String.valueOf(dIn.readInt())};
+            String[] accountInfo = accountNumber.split("-");
+
+            if (requestType == "balance" || requestType == "withdraw") {
+                dOut.writeUTF(requestType);
+                generateRequestJson(requestType, accountInfo, pin);
+
+                JSONObject input = new JSONObject(dIn.readUTF());
+                String[] code = StringEscapeUtils.escapeJava(input.getJSONObject("body").getString("code"));
+
+                if (code[0] == "200") {
+                    return input.toString();
+                } else {
+                    return code;
+                }
+
+            } else {
+                return errorMsg;
             }
         } catch (Exception e) {
             return errorMsg;
         }
+
         return errorMsg;
+    }
+
+    static private void generateRequestJson(String requestType, String[] accountInfo, String pin) {
+        JSONObject json = new JSONObject();
+
+        JSONObject body = new JSONObject();
+
+        body.put("account", accountInfo[2]);
+        body.put("pin", pin);
+
+        json.put("header", generateResponseHeader(requestType, accountInfo));
+        json.put("body", body);
+
+        sendResponse(json);
+    }
+
+    static private JSONObject generateResponseHeader(String requestType, String[] accountInfo) {
+        JSONObject header = new JSONObject();
+        header.put("originCountry", "US");
+        header.put("originBank", "EVIL");
+        header.put("receiveCountry", accountInfo[0]);
+        header.put("receiveBank", accountInfo[1]);
+        header.put("action", requestType);
+        return header;
+    }
+
+    static private void sendResponse(JSONObject json) {
+        dOut.writeUTF(json.toString());
     }
 }
