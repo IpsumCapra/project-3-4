71722c051241f68c7856450681fd7b5b9c544e44
Daylan <daylandelange@gmail.com>
2020-06-06 15:38:06 +0200

    Revert "fixed bug in resetvars"
    This reverts commit 6e0eba50eba65a5cbb29b8aad892a8629ef049d0.

--- ATM-Ardu/src/main.cpp
+++ ATM-Ardu/src/main.cpp
@@ -308,9 +308,9 @@ void resetVars() {
     blocked = false;
     for (int i = 0; i < 17; i++)
     {
-        sendArray[i] = 'x';
+        sendArray[i] = '';
     }
-    inKey = 'x';
+    inkey = '';
     delay(5000);
 }
 
